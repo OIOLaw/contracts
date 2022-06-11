9e5c5602e15693cef3fad3a5fe134f913351c832
Tomáš Martykán <t.martykan@gmail.com>
2022-06-11 13:06:12 +0200

    Test token auto mint

--- src/test/TestToken.sol
+++ src/test/TestToken.sol
@@ -6,7 +6,9 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TestToken is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("MyToken", "MTK") {}
+    constructor() ERC20("MyToken", "MTK") {
+        _mint(msg.sender, 1e22);
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
